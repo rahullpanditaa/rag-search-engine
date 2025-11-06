0ba2f90eb2311f81422b8ff6fe0e6de29597c93e
Rahul Pandita <rpandita1999@outlook.com>
2025-11-06 02:38:09 +0000

    create a SemanticSearch class, create command, function to verify/print details about embedding model being used

---
+++ cli/lib/semantic_search.py
@@ -0,0 +1,10 @@
+from sentence_transformers import SentenceTransformer
+
+class SemanticSearch:
+    def __init__(self):
+        self.model = SentenceTransformer('all-MiniLM-L6-V2')
+
+def verify_model():
+    sem_search = SemanticSearch()
+    print(f"Model loaded: {sem_search.model}")
+    print(f"Max sequence length: {sem_search.model.max_seq_length}")
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 
 import argparse
+from lib.semantic_search import verify_model
 
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
+    subparsers = parser.add_subparsers(dest="command", help="Available semantic search commands")
+
+    verify_parser = subparsers.add_parser("verify", help="Print information about embedding model used.")
     args = parser.parse_args()
 
     match args.command:
+        case "verify":
+            verify_model()
         case _:
             parser.print_help()
 
