7f37e7baa75745b30a5bdf69ccf873ff1301e5fa
Rahul Pandita <rpandita1999@outlook.com>
2025-11-06 04:05:32 +0000

    create a method in SemanticSearch to embed text; create command, function to embed input text

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -4,7 +4,23 @@ class SemanticSearch:
     def __init__(self):
         self.model = SentenceTransformer('all-MiniLM-L6-V2')
 
+    def generate_embedding(self, text: str):
+        if text == "" or text.isspace():
+            raise ValueError("Given text is empty or contains only whitespce")
+        
+        embeddings = self.model.encode([text])
+        return embeddings[0]
+
+
+
 def verify_model():
     sem_search = SemanticSearch()
     print(f"Model loaded: {sem_search.model}")
     print(f"Max sequence length: {sem_search.model.max_seq_length}")
+
+def embed_text(text: str):
+    sem_search = SemanticSearch()
+    embedding = sem_search.generate_embedding(text=text)
+    print(f"Text: {text}")
+    print(f"First 3 dimensions: {embedding[:3]}")
+    print(f"Dimensions: {embedding.shape[0]}")
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python3
 
 import argparse
-from lib.semantic_search import verify_model
+from lib.semantic_search import verify_model, embed_text
 
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available semantic search commands")
 
     verify_parser = subparsers.add_parser("verify", help="Print information about embedding model used.")
+    
+    embed_text_parser = subparsers.add_parser("embed_text", help="Generates an embedding for given string")
+    embed_text_parser.add_argument("text", type=str, help="Single string argument to generate embedding for.")
+    
     args = parser.parse_args()
 
     match args.command:
         case "verify":
             verify_model()
+
+        case "embed_text":
+            embed_text(args.text)
         case _:
             parser.print_help()
 
